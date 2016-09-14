b91373b316dc31a36a46b60662838f037b64348a
= <=>
2016-09-14 10:01:24 -0500

    Added some comments

--- constants.py
+++ constants.py
@@ -3,10 +3,13 @@
 Created on Mon Sep 12 17:32:25 2016
 
 @author: Tiny
+
+This file contains the physical constants required for simulation
+All the units are in S.I.
 """
 
-q = 1.60217662e-19
-kB = 1.38064852e-23
-me = 9.10938356e-31
-h = 6.62607004e-34
-eps0 =  8.854187817e-12
+q = 1.60217662e-19          # Charge of electron (C)
+kB = 1.38064852e-23         # Boltzmann constant (kg.m^2/k/s^2)
+me = 9.10938356e-31         # Mass of electron (kg)
+h = 6.62607004e-34          # Planck's constant (kg.m^2/s)
+eps0 =  8.854187817e-12     # Vacuum permittivity (F/m)
--- main.py
+++ main.py
@@ -5,3 +5,5 @@ Created on Mon Sep 12 16:26:03 2016
 @author: Sarkar Anwar
 """
 
+import numpy as np
+import constants

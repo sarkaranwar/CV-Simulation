7b71b5c7c2c214adccfd206d3f8f575fbee97374
= <=>
2016-10-09 14:55:12 -0500

    Added semiconductor

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 Created on Mon Sep 12 16:26:03 2016
 
 @author: Sarkar Anwar
+@e-mail: sarkar.rm.anwar@gmail.com
 """
 
 import numpy as np
@@ -10,48 +11,47 @@ from scipy import interpolate
 import constants as const
 import matplotlib.pyplot as plt
 from fermi_half import fermi_half_numeric, fermi_half_approx
-from time import sleep
-#import os
+from semiconductor import semiconductor as sc
 
 
-#os.system('cls')
 
 
+x = sc("Silicon", 300)
+
+
+""" Define simulation parameters """
+max_iter = 500
+error = 1e-9
+verbose = True
+
+
+"""Define device parameters """
 T = 300			# Temperature in Kelvin
 Ndop = 1e17		# Doping density in /cm3
 tox = 2			# EOT in nm
 sc_type = 'p'	# Semiconductor type
-
 V_FB = 0.0
-
 fermi_option = "numeric"
-verbose = 0
 
-#print("Setup:")
-#print("Temperature:", T, "K")
-#print("Doping Density:", Ndop, "/cm3")
-#print("EOT", tox, "nm")
-
-phi = np.linspace(-2,2,201)
-#psi_s = np.linspace(-0.2,1.1,101)
 Vg_exp = np.linspace(-2,2,101)
 
-Eg = 1.12         	# Bandgap in eV
-Nc = 3.2e19       	# Conduction band density of states /cm3
-Nv = 2e19         	# Valence band density of states /cm3
-ni = 1e10		    # Intrinsic carrier concentration in /cm3
-epsr_s = 11.7		# Silicon dielectric constant
+"""Define semiconductor parameters """
+Eg = 1.12               # Bandgap in eV
+Nc = 3.2e19             # Conduction band density of states /cm3
+Nv = 2e19               # Valence band density of states /cm3
+ni = 1e10               # Intrinsic carrier concentration in /cm3
+epsr_s = 11.7           # Silicon dielectric constant
 epsr_ox = 3.9
 
-Ndop = 1e6 * Ndop	# Doping density in /m3
-Nc = 1e6 * Nc       # Conduction band density of states /m3
-Nv = 1e6 * Nv       # Valence band density of states /m3
-tox = 1e-9 * tox	# EOT in m
-ni = 1e6 * ni       # Intrinsic carrier concentration in /m3
+"""Convert semiconductor parameters to SI unit"""
+Ndop = 1e6 * Ndop	       # Doping density in /m3
+Nc = 1e6 * Nc           # Conduction band density of states /m3
+Nv = 1e6 * Nv           # Valence band density of states /m3
+tox = 1e-9 * tox        # EOT in m
+ni = 1e6 * ni           # Intrinsic carrier concentration in /m3
 
+""" Calculate some regularly used constants """
 eps_s = epsr_s * const.eps0
-#print(eps_s)
-
 Vt = const.kB * T / const.q
 
 if fermi_option == "numeric":
@@ -59,9 +59,10 @@ if fermi_option == "numeric":
 else :
     fermi = np.vectorize(fermi_half_approx)
 
-#print("Thermal voltage- ",Vt)
-n = 2*Nc/np.sqrt(np.pi) * fermi((phi - Eg/2)/Vt)
-p = 2*Nv/np.sqrt(np.pi) * fermi((-phi - Eg/2)/Vt)
+""" Calculate Fermi Dirac distribution of carriers for a range of potential for easy interpolation """
+phi_temp = np.linspace(-2,2,201)
+n = 2*Nc/np.sqrt(np.pi) * fermi((phi_temp - Eg/2)/Vt)
+p = 2*Nv/np.sqrt(np.pi) * fermi((-phi_temp - Eg/2)/Vt)
 
 if sc_type == 'n':
     Nd = Ndop
@@ -71,55 +72,32 @@ else:
     Na = Ndop
 
 total_carrier = p-n+Nd-Na
-#plt.semilogy(phi,np.abs(total_carrier))
 
-carrier_2_phi = interpolate.interp1d(total_carrier,phi,assume_sorted = False)
-phi_2_carrier = interpolate.interp1d(phi,total_carrier,assume_sorted = False)
+""" Define interpolation functions for easy interpolation """
+carrier_2_phi = interpolate.interp1d(total_carrier,phi_temp,assume_sorted = False)
+phi_2_carrier = interpolate.interp1d(phi_temp,total_carrier,assume_sorted = False)
+
+""" Calculate bulk potential """
 phi_b = carrier_2_phi(0)
-print('Bulk potential is ',phi_b, 'eV')
-
-#plt.semilogy(phi,n1)
-#plt.semilogy(phi,n2)
-#plt.semilogy(phi,abs(total_carrier))
-#plt.xlabel('Potential')
-#plt.ylabel('Carrier density (/m3)')
-#plt.show()
 
 
-# Define Grid
-
+""" Define Grid """
 a = 0.21e-9         # Atomic radius in m
 xmin = 0
 xmax = 0.5e-7
 nx = 1000
 dx = (xmax-xmin)/nx
-
 x = np.arange(xmin,xmax,dx)
-#print(x)
-
 print("Number of grid points is ",np.size(x))
-#"""
+
+""" Define Surface potential """
+phi_start = -1
+phi_end = 1
+phi_n = 2
+phi_s = np.linspace(phi_start,phi_end,phi_n)
+
 phi_x = np.zeros(np.size(x))
-n_x = np.zeros(np.size(x))
-p_x = np.zeros(np.size(x))
 carrier_x = np.zeros(np.size(x))
-Nd_x = Nd * np.ones(np.size(x))
-Na_x = Na * np.ones(np.size(x))
-
-phi_s = np.linspace(-1,1,2)
-
-max_iter = 500
-
-
-#plt.ion()
-#fig = plt.figure()
-#ax1 = fig.add_subplot(211)
-#ax2 = fig.add_subplot(212)
-#ax1.set_ylim([-2,2])
-#ax2.set_ylim([-1e23,1e23])
-#line1, = ax1.plot(x, phi_x, 'r-')
-#line2, = ax2.plot(x,carrier_x, 'b-')
-
 
 for phi in phi_s:
     print("Solving for phi_s = ",phi)
@@ -145,10 +123,25 @@ for phi in phi_s:
         phi_x0 = phi_x
         carrier0 = carrier_x
         if ((error_phi<1e-19) and (error_carrier<1e-9)):
+            print("Error")
             break
         i = i+1
-    plt.subplot(211)        
-    plt.plot(x,phi_x)
-    plt.subplot(212)
-    plt.plot(x,carrier_x)
+    if verbose:
+        plt.subplot(211)        
+        plt.plot(x,phi_x)
+        plt.subplot(212)
+        plt.plot(x,carrier_x)
 plt.show()
+
+if verbose:
+    print('Setup:')
+    print('Temperature: ',T, 'K')
+    print('Doping density: ',Ndop/1e6, '/cm3')
+    print('EOT: ',tox/1e-9, 'nm')
+    print('Type: ',sc_type)
+    print('Flat band voltage: ',V_FB, 'V')
+    print('Semiconductor parameter: ')
+    print('Bandgap: ',Eg, 'eV')
+    print('CB DOS: ',Nc/1e6, '/cm3')    
+    print('VB DOS: ',Nv/1e6, '/cm3')    
+    print('Bulk potential: ',phi_b, 'eV')

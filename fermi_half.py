966890dcbaf39b9554c7155aeaa86d376961e304
= <=>
2016-10-09 11:09:15 -0500

    Added Poisson Solver

--- fermi_half.py
+++ fermi_half.py
@@ -4,12 +4,22 @@ Created on Wed Sep 14 06:22:46 2016
 
 @author: Sarkar Anwar
 @e-mail: sarkar.rm.anwar@gmail.com
-"""
-import numpy as np
 
-def fermi_half(eta):
+Returns the electron or hole density for a given fermi level
+
+"""
+from numpy import exp, sqrt, trapz, arange, pi
+
+def fermi_half_approx(eta):
+#    https://nanohub.org/resources/5475
     x = eta
-    mu = x**4 + 50 + 33.6*x*( 1 - 0.68*np.exp(-0.17*(x+1)**2))
-    xi = 3*np.sqrt(np.pi)/(4*mu**(3/8))
-    y = (np.exp(-x)+xi)**-1
+    mu = x**4 + 50 + 33.6*x*( 1 - 0.68*exp(-0.17*(x+1)**2))
+    xi = 3*sqrt(pi)/(4*mu**(3/8))
+    y = (exp(-x)+xi)**-1
     return y
+    
+def fermi_half_numeric(eta):
+    xi = arange(0,300,0.01)
+    y = sqrt(xi)/(1+exp(xi - eta))
+    f = trapz(y,xi)
+    return f
--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import numpy as np
 from scipy import interpolate
 import constants as const
 import matplotlib.pyplot as plt
-from fermi_half import fermi_half
-
+from fermi_half import fermi_half_numeric, fermi_half_approx
+from time import sleep
 #import os
 
 
@@ -18,19 +18,21 @@ from fermi_half import fermi_half
 
 
 T = 300			# Temperature in Kelvin
-Ndop = 1e16		# Doping density in /cm3
+Ndop = 1e17		# Doping density in /cm3
 tox = 2			# EOT in nm
 sc_type = 'p'	# Semiconductor type
 
 V_FB = 0.0
 
+fermi_option = "numeric"
+verbose = 0
 
 #print("Setup:")
 #print("Temperature:", T, "K")
 #print("Doping Density:", Ndop, "/cm3")
 #print("EOT", tox, "nm")
 
-phi = np.linspace(-2,2,401)
+phi = np.linspace(-2,2,201)
 #psi_s = np.linspace(-0.2,1.1,101)
 Vg_exp = np.linspace(-2,2,101)
 
@@ -48,13 +50,18 @@ tox = 1e-9 * tox	# EOT in m
 ni = 1e6 * ni       # Intrinsic carrier concentration in /m3
 
 eps_s = epsr_s * const.eps0
-
+#print(eps_s)
 
 Vt = const.kB * T / const.q
 
+if fermi_option == "numeric":
+    fermi = np.vectorize(fermi_half_numeric)
+else :
+    fermi = np.vectorize(fermi_half_approx)
+
 #print("Thermal voltage- ",Vt)
-n = 2*Nc/np.sqrt(np.pi) * fermi_half((phi - Eg/2)/Vt)
-p = 2*Nv/np.sqrt(np.pi) * fermi_half((-phi - Eg/2)/Vt)
+n = 2*Nc/np.sqrt(np.pi) * fermi((phi - Eg/2)/Vt)
+p = 2*Nv/np.sqrt(np.pi) * fermi((-phi - Eg/2)/Vt)
 
 if sc_type == 'n':
     Nd = Ndop
@@ -66,14 +73,14 @@ else:
 total_carrier = p-n+Nd-Na
 #plt.semilogy(phi,np.abs(total_carrier))
 
-#phi_b = np.interp(1e24,total_carrier,phi)
-f_carrier_phi = interpolate.interp1d(total_carrier,phi,assume_sorted = False)
-f_phi_carrier = interpolate.interp1d(phi,total_carrier,assume_sorted = False)
-phi_b = f_carrier_phi(0)
+carrier_2_phi = interpolate.interp1d(total_carrier,phi,assume_sorted = False)
+phi_2_carrier = interpolate.interp1d(phi,total_carrier,assume_sorted = False)
+phi_b = carrier_2_phi(0)
 print('Bulk potential is ',phi_b, 'eV')
 
-#plt.semilogy(phi_s,n)
-#plt.semilogy(phi_s,p)
+#plt.semilogy(phi,n1)
+#plt.semilogy(phi,n2)
+#plt.semilogy(phi,abs(total_carrier))
 #plt.xlabel('Potential')
 #plt.ylabel('Carrier density (/m3)')
 #plt.show()
@@ -83,26 +90,65 @@ print('Bulk potential is ',phi_b, 'eV')
 
 a = 0.21e-9         # Atomic radius in m
 xmin = 0
-xmax = 1e-6
-x = np.arange(xmin,xmax,a)
-#print(np.size(x))
+xmax = 0.5e-7
+nx = 1000
+dx = (xmax-xmin)/nx
 
+x = np.arange(xmin,xmax,dx)
+#print(x)
+
+print("Number of grid points is ",np.size(x))
+#"""
 phi_x = np.zeros(np.size(x))
-#print(np.size(phi_x))
+n_x = np.zeros(np.size(x))
+p_x = np.zeros(np.size(x))
+carrier_x = np.zeros(np.size(x))
+Nd_x = Nd * np.ones(np.size(x))
+Na_x = Na * np.ones(np.size(x))
 
-phi_s = np.linspace(-2,2,10)
+phi_s = np.linspace(-1,1,2)
+
+max_iter = 500
+
+
+#plt.ion()
+#fig = plt.figure()
+#ax1 = fig.add_subplot(211)
+#ax2 = fig.add_subplot(212)
+#ax1.set_ylim([-2,2])
+#ax2.set_ylim([-1e23,1e23])
+#line1, = ax1.plot(x, phi_x, 'r-')
+#line2, = ax2.plot(x,carrier_x, 'b-')
+
 
 for phi in phi_s:
-    phi_x0 = np.linspace(phi,phi_b,np.size(x))
-    carrier0 = f_phi_carrier(phi_x0)
-#    print(np.size(carrier0))
-#    plt.plot(x,carrier0)
-    
+    print("Solving for phi_s = ",phi)
+    phi_x0 = np.linspace(phi,phi_b,nx)
+    carrier0 = phi_2_carrier(phi_x0)
+    phi_x0[1:nx-1] = phi_b    
+    phi_x0[0] = phi
+    i = 0
 
-
-
-
-
-
-
-
+    while (i<max_iter):
+        #Solve poisson equation
+        phi_x[0] = phi
+        for idx, xi in enumerate(x):        
+#            phi_x[1:nx-2] = (phi_x0[0:nx-3] + phi_x0[2:nx-1] + dx * dx * const.q*carrier0[1:nx-2]/eps_s)/2 #does not work
+            if 0<idx<nx-1:
+                phi_x[idx] = (phi_x0[idx-1] + phi_x0[idx+1] + dx**2 * const.q * carrier0[idx]/eps_s)/2
+        phi_x[nx-1] = phi_b
+        carrier_x = phi_2_carrier(phi_x)
+        
+        #Calculatue error        
+        error_carrier = np.max((np.abs(carrier0 - carrier_x))/carrier0)
+        error_phi = np.max(np.abs(phi_x0 - phi_x))
+        phi_x0 = phi_x
+        carrier0 = carrier_x
+        if ((error_phi<1e-19) and (error_carrier<1e-9)):
+            break
+        i = i+1
+    plt.subplot(211)        
+    plt.plot(x,phi_x)
+    plt.subplot(212)
+    plt.plot(x,carrier_x)
+plt.show()
